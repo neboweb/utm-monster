b691c1facb5283c79181fd01c4dac1004c7d9d05
Stafford Brooke <stafford@inquus.com>
2010-06-30 23:54:51 -0400

    singularity

--- utmm.js
+++ utmm.js
@@ -1,29 +1,22 @@
-jQuery.utm = function(utm_values) {
- 
-  var _get_cookies = function()
+jQuery.utm = function() {
+  if (document.cookie && document.cookie != '')
   {
-    if (document.cookie && document.cookie != '')
+    var utm_vals = {}
+    var cookies = document.cookie.split(';');
+    for (var i = 0; i < cookies.length; i++)
     {
-      var utm_vals = {}
-      var cookies = document.cookie.split(';');
-      for (var i = 0; i < cookies.length; i++)
+      var cookie = decodeURIComponent($.trim(cookies[i]));
+      if (cookie.substring(0, 7) == "__utmz=")
       {
-        var cookie = decodeURIComponent($.trim(cookies[i]));
-        if (cookie.substring(0, 7) == "__utmz=")
+        $.each(cookie.match(/utmc[a-z]*=\(?[a-z]*\)?/g), function(i, part)
         {
-          $.each(cookie.match(/utmc[a-z]*=\(?[a-z]*\)?/g), function(i, part)
-          {
-            var pieces = part.split('=');
-            var name =pieces[0];
-            var value = pieces[1];
-            utm_vals[name] = value;
-          })
-        }
+          var pieces = part.split('=');
+          var name =pieces[0];
+          var value = pieces[1];
+          utm_vals[name] = value;
+        })
       }
-      return utm_vals;
     }
-   
   }
-
-  return _get_cookies;
+  return utm_vals;
 }
